bf887c797913d4cd5100ff90c3e0634b2b6c6bec
=rfkih23 <=rifkihidayat23@yahoo.com>
2021-12-31 21:49:29 +0700

    filter

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,22 +9,35 @@ function Index() {
     
 
     const [products, setProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([])
 
     useEffect( async () =>{
         axios.get("http://localhost:2021/products")
         .then((res) => {
           setProducts(res.data);
+          setFilteredProducts(res.data)
         })
         .catch((error) => {
 
         })
-    },[])
+    },[]);
+
+    const onSearchProducts = (keyword)=> {
+      const filterResult = products.filter((product)=>{
+       const productLowerCase = product.productName.toLowerCase()
+       const keywordLowerCase = keyword.toLowerCase()
+       
+       return productLowerCase.includes(keywordLowerCase)
+      })
+
+      setFilteredProducts(filterResult)
+    }
 
     console.log(products);
     return (
         <div className='row container mt-5'>
-            <ProductManager/>
-            <ListProduct products={products}/>
+            <ProductManager onSearchProducts={onSearchProducts}/>
+            <ListProduct products={filteredProducts}/>
         </div>
     )
 }
