cadaa34f24491dafa3178256eb7c045774bc47da
=rfkih23 <=rifkihidayat23@yahoo.com>
2022-01-03 15:52:03 +0700

    dsaf

--- src/pages/ManageProduct/Index.jsx
+++ src/pages/ManageProduct/Index.jsx
@@ -5,22 +5,55 @@ import "./style.css"
 
 function ManageProduct() {
     const [products, setProducts] = useState ([]);
+    const [formState, setFormState] = useState ({
+      productName:"",
+      productImage:"",
+      price: 0,
+      description: "",
+      category:"",
+    });
+
+    const handleChange =(e) => {
+      setFormState({...formState, [e.target.name]: e.target.value})
+    };
+
+    const fetchProducts = () =>{
+      axios.get("/products")
+      .then((res) => {
+        const {data} = res
+        setProducts(data);
+      })
+      .catch((error) => {
+        alert(error.message)
+      });
+    }
     
     useEffect(() =>{
-        axios.get("/products")
-        .then((res) => {
-          const {data} = res
-          setProducts(data);
-        })
-        .catch((error) => {
-          alert(error.message)
-        });
+       fetchProducts();
     }, []);
 
     const renderProducts = () => {
       return products.map((product) =><ProductList product={product}/>);
     };
+    
 
+    const addNewProduct=() =>{
+      const {productImage, price, productName, category, description} = formState;
+
+      const newProduct ={
+        id: new Date().getTime(),
+        productImage, 
+        price, 
+        productName, 
+        category, 
+        description
+      };
+      axios
+        .post("/products" ,newProduct)
+        .then(res => console.log({res}))
+        .catch((error) => console.log({error}));
+      fetchProducts()
+    };
 
   return ( 
     <div className="container">
@@ -48,36 +81,45 @@ function ManageProduct() {
             <td>
               <input 
                 placeholder='Product Name'
-                name="addProductName"
+                name="productName"
                 type ="text"
                 className= "formcontrol" 
+                onChange={handleChange}
                 />
               </td>
             <td>
               <input 
                 placeholder='Price'
-                name ="addPrice" 
+                name ="price" 
                 type ="number" 
-                className='formcontrol'/>
+                className='formcontrol'
+                onChange={handleChange}
+                />
             </td>
             <td>
               <input 
                 placeholder='Image'
-                name="addProductImage"
+                name="productImage"
                 type="text"
                 className="form-control"
+                onChange={handleChange}
               />
             </td>
             <td>
                 <input 
                   placeholder='Description'
-                  name="addDescription"
+                  name="description"
                   type="text"
                   className="form-control"
+                  onChange={handleChange}
                 />
             </td>
             <td>
-              <select name ="addCategory" className='form-control'>
+              <select 
+                name ="category" 
+                className='form-control ' 
+                onChange={handleChange}
+                >
                 <option value="">All Items</option>
                 <option value="kaos">Kaos</option>
                 <option value="celana">Celana</option>
@@ -85,7 +127,7 @@ function ManageProduct() {
               </select>
             </td>
             <td colSpan="2">
-              <button className='btn btn-info'>Add Product</button>
+              <button onClick={addNewProduct} className='btn btn-info'>Add Product</button>
             </td>
           </tr>
         </tfoot>
