2a897e0fcfdc996ea1ed41881cfb5c050c088780
=rfkih23 <=rifkihidayat23@yahoo.com>
2022-02-05 12:12:28 +0700

    login useselector

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,31 +1,68 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import{Navbar,NavbarBrand,Nav,NavItem,NavLink} from 'reactstrap'
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+  } from "reactstrap";
 import { useSelector, useDispatch} from 'react-redux' 
 
 
 function index() {
-   const username =  useSelector((state) =>{
-        return state.auth.username;
-    })
+    const { username, role } = useSelector((state) => {
+        return state.auth;
+      });
 
     console.log(`navigation : ${username} `);
     return (
         <div>
             <Navbar color="light" light expand="md">
                 <NavbarBrand href="/">EMMERCE</NavbarBrand>
-                <Nav className="ms-auto" navbar>
-                    <NavItem>
-                        <NavLink tag={Link} to="/manage-product"> 
-                            Admin 
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink tag={Link} to="/login"> 
-                            Login
-                        </NavLink>
-                    </NavItem>
-                </Nav>
+                <Nav className="me-auto" navbar>
+        {username ? (
+          <UncontrolledDropdown inNavbar nav>
+            <DropdownToggle caret nav>
+              HELLO {username}
+            </DropdownToggle>
+            <DropdownMenu end>
+              <DropdownItem tag={Link} to="/cart">
+                Cart
+              </DropdownItem>
+              <DropdownItem tag={Link} to="/history-transaction">
+                Transaction
+              </DropdownItem>
+              {role === "admin" && (
+                <DropdownItem tag={Link} to="/manage-products">
+                  Admin
+                </DropdownItem>
+              )}
+              <DropdownItem divider />
+              <DropdownItem >Logout</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        ) : (
+          <>
+            <NavItem>
+              <NavLink tag={Link} to="/login">
+                Login
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/register">
+                Register
+              </NavLink>
+            </NavItem>
+          </>
+        )}
+      </Nav>   
+
+
             </Navbar>
         </div>
     )
